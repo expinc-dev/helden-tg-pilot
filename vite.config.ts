d36b7608180b11d19fe005beda430fbd48ae6ed9
khairulumam92 <khairulumamku92@gmail.com>
2026-09-12 14:22:39 +0700

    chore: remove @vitejs/plugin-basic-ssl (dev server back to http)

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import tailwindcss from '@tailwindcss/vite'
-import basicSsl from '@vitejs/plugin-basic-ssl'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), basicSsl()],
+  plugins: [react(), tailwindcss()],
   resolve: { alias: { '@': path.resolve(__dirname, './src') } },
   server: { port: 3001, open: true, host: true },
 })
